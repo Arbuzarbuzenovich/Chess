4f55118d6e873765fdce980007a2aac71b19f9e7
Arbuzarbuzenovich <nik.bogdanov.207@gmail.com>
2026-03-22 23:52:31 +0300

    Create auth_service.py
    Аутентификация

---
+++ src/auth/auth_service.py
@@ -0,0 +1,6 @@
+class AuthService:
+    def register(self, username, password):
+        return "Пользователь зарегистрирован"
+
+    def login(self, username, password):
+        return "Вход выполнен"
