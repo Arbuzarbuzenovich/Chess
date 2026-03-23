4ec2fde76fc4c8f510ac83ac7472e340061b5fe4
Arbuzarbuzenovich <nik.bogdanov.207@gmail.com>
2026-03-23 03:20:02 +0300

    dock

--- src/game/chess_ai.py
+++ src/game/chess_ai.py
@@ -1,3 +1,36 @@
+"""Модуль искусственного интеллекта."""
+
 class ChessAI:
+    """Класс компьютерного соперника.
+
+    Attributes:
+        difficulty (str): Уровень сложности ИИ.
+    """
+
+    def __init__(self, difficulty="medium"):
+        """Инициализирует шахматный искусственный интеллект.
+
+        Args:
+            difficulty (str): Уровень сложности.
+        """
+        self.difficulty = difficulty
+
     def choose_move(self):
+        """Выбирает следующий ход для компьютерного соперника.
+
+        Returns:
+            str: Ход, выбранный ИИ.
+        """
         return "e2-e4"
+
+    def set_difficulty(self, difficulty):
+        """Устанавливает новый уровень сложности.
+
+        Args:
+            difficulty (str): Новый уровень сложности.
+
+        Returns:
+            str: Текущий уровень сложности.
+        """
+        self.difficulty = difficulty
+        return self.difficulty
